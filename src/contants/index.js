92df13f4153f036057417ba48ea6075cce43298e
TahaY291 <mtahayasin07@gmail.com>
2024-11-21 12:26:02 +0500

    Final changes before deployment

---
+++ src/contants/index.js
@@ -0,0 +1,161 @@
+export const features = [
+  {
+    id: "0",
+    icon: "/images/feature-1.png",
+    caption: "Easy integration",
+    title: "Work smarter not harder",
+    text: "With Xora, tedious tasks are history. Automation and smart processes bring your productivity to new heights. It's like having an extra cup of coffee, but without the jitters.",
+    button: {
+      btnIcon: "/images/magictouch.svg",
+      title: "Watch the demo",
+    },
+  },
+  {
+    id: "1",
+    icon: "/images/feature-2.png",
+    caption: "Secure & trustworthy",
+    title: "Sleep easy, we got your back",
+    text: "Your data security is our priority. With state-of-the-art encryption and robust privacy controls, Xora helps keeps your information secure and locked up tighter than Fort Knox.",
+    button: {
+      btnIcon: "/images/docs.svg",
+      title: "Read the docs",
+    },
+  },
+];
+
+export const details = [
+  {
+    id: "0",
+    icon: "/images/detail-1.png",
+    title: "AI automated video editing",
+  },
+  {
+    id: "1",
+    icon: "/images/detail-2.png",
+    title: "Collaborate with your team",
+  },
+  {
+    id: "2",
+    icon: "/images/detail-3.png",
+    title: "Ultra fast cloud-engine",
+  },
+  {
+    id: "3",
+    icon: "/images/detail-4.png",
+    title: "24 / 7 Customer support",
+  },
+];
+
+export const plans = [
+  {
+    id: "0",
+    title: "Core",
+    priceMonthly: 19,
+    priceYearly: 12,
+    caption: "Best for solo creators",
+    features: [
+      "100MB Cloud storage",
+      "100+ prompt templates",
+      "5 projects",
+      "24/7 support",
+    ],
+    icon: "/images/circle.svg",
+    logo: "/images/plan-1.png",
+  },
+  {
+    id: "1",
+    title: "Overdrive",
+    priceMonthly: 79,
+    priceYearly: 59,
+    caption: "Most popular plan",
+    features: [
+      "All Starter features",
+      "1TB additional storage",
+      "Unlimited projects",
+      "Analytics",
+    ],
+    icon: "/images/triangle.svg",
+    logo: "/images/plan-2.png",
+  },
+  {
+    id: "2",
+    title: "Team",
+    priceMonthly: 39,
+    priceYearly: 29,
+    caption: "Exclusively for teams",
+    features: [
+      "All Overdrive features",
+      "10TB additional storage",
+      "50% off per member",
+      "Real-time collaboration",
+    ],
+    icon: "/images/hexagon.svg",
+    logo: "/images/plan-3.png",
+  },
+];
+export default plans;
+
+export const logos = [
+  {
+    id: "0",
+    title: "Afterpay",
+    url: "/images/logos/afterpay.svg",
+    width: 156,
+    height: 48,
+  },
+  {
+    id: "1",
+    title: "Amplitude",
+    url: "/images/logos/amplitude.svg",
+    width: 194,
+    height: 48,
+  },
+  {
+    id: "2",
+    title: "Sonos",
+    url: "/images/logos/sonos.svg",
+    width: 115,
+    height: 48,
+  },
+  {
+    id: "3",
+    title: "Maze",
+    url: "/images/logos/maze.svg",
+    width: 142,
+    height: 48,
+  },
+  {
+    id: "4",
+    title: "Drips",
+    url: "/images/logos/drips.svg",
+    width: 77,
+    height: 48,
+  },
+];
+
+export const links = [
+  {
+    id: "0",
+    title: "Ios",
+    icon: "../../public/images/first.svg",
+    url: "#",
+  },
+  {
+    id: "1",
+    title: "Android",
+    icon: "../../public/images/scnd.svg",
+    url: "#",
+  },
+  {
+    id: "2",
+    title: "Windows",
+    icon: "../../public/images/thrd.svg",
+    url: "#",
+  },
+  {
+    id: "3",
+    title: "Web",
+    icon: "../../public/images/frth.svg",
+    url: "#",
+  },
+];
